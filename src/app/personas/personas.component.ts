8430a38e980f05a022831839d1800b1db36532ee
RuloLara <laragra96@gmail.com>
2021-02-08 14:46:58 -0600

    prueba

---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+   selector: 'app-personas' ,
+   templateUrl: './personas.component.html',
+   styleUrls: ['./personas.component.css']
+})
+export class PersonasComponent{
+   deshabilitar = false; 
+   mensaje = 'No se ha agregado ninguna persona';
+   titulo ='123123';
+   titulo2='Maestro';
+   agregarPersona(){
+      this.mensaje = 'Persona agregada';
+
+   }
+   
+
+   modificarTitulo(event: Event){
+      this.titulo = (<HTMLInputElement>event.target).value;
+   }
+
+}
